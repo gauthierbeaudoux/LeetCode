4a76670004b8e8587ea3e58a8c734e2d503b055a
Gauthier <gauthierbeaudoux@gmail.com>
2023-11-22 15:17:48 +0100

    1317

--- 1317_ConvertIntToSum.py
+++ 1317_ConvertIntToSum.py
@@ -1,17 +1,12 @@
 import math
-n = 124
+n = 11
 
 
 def getNoZeroIntegers(n: int) -> list[int]:
-    m = n%10
-    i = int(math.log10(n))
-    # print(i)
-    # print(m)
-    if i == 0:
-        return [1, n-1]
-    if m == 9:
-        return [10*i + m - 1, n - (10*i + m - 1)]
-    return [10**i - m - 1, n - (10**i - m - 1)]
-
+    i = 1
+    while True:
+        if not ('0' in list(str(n-i)) or '0' in (list(str(i)))):
+            return [n-i, i]
+        i += 1
 
 print(getNoZeroIntegers(n))
