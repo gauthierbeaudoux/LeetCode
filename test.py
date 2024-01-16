924e3e17abd30ce4f62e62a51107f91a3afa62db
Gauthier <gauthierbeaudoux@gmail.com>
2024-01-16 21:52:33 +0100

    380

--- test.py
+++ test.py
@@ -7,27 +7,8 @@ food = ["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"]
 
 
 
-dic_node = {1: [5, 3], 5: [4], 4: [9, 2], 3: [10, 6]}
-print(dic_node)
-list_infected = [3]
-# while len(dic_node) > 0:
-x = list_infected[0]
-list_infected.pop(0)
-if x in dic_node:
-    for value in dic_node[x]:
-        list_infected.append(value)
-    del dic_node[x]
-list_to_delete = []
-for key, value in dic_node.items():
-    if x in value:
-        list_to_delete.append(key)
-        list_infected.append(key)
-        for y in value:
-            if y != x:
-                list_infected.append(y)
-for i in list_to_delete:
-    del dic_node[i]
+test_set = set()
 
-print(dic_node)
-
-print(list_infected)
+test_set.add(4)
+test_set.add(6)
+print(test_set)
