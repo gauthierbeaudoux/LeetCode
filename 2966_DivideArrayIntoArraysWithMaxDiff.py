98c5ee320d4f5e8bd762a110186558982d0b6b63
Gauthier <gauthierbeaudoux@gmail.com>
2024-02-01 15:24:55 +0100

    Create 2966_DivideArrayIntoArraysWithMaxDiff.py

---
+++ 2966_DivideArrayIntoArraysWithMaxDiff.py
@@ -0,0 +1,18 @@
+nums = [1,3,4,8,7,9,3,5,1]
+k = 2
+
+def divideArray(nums: list[int], k: int) -> list[list[int]]:
+    n = len(nums)
+    result = []
+    if n % 3 > 0:
+        return result
+    nums.sort()
+    for i in range(0,n,3):
+        L = nums[i:i+3]
+        if max(L)-min(L) > k:
+            return []
+        result.append(L)
+    return result
+
+
+print(divideArray(nums,k))
