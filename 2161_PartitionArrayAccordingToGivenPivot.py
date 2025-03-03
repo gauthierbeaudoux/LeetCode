cbf4eb472f006e2c208de1aa645295559e0037c9
Gauthier <gauthierbeaudoux@gmail.com>
2025-03-03 22:15:58 +0800

    Update 2161_PartitionArrayAccordingToGivenPivot.py

--- 2161_PartitionArrayAccordingToGivenPivot.py
+++ 2161_PartitionArrayAccordingToGivenPivot.py
@@ -3,45 +3,23 @@ pivot = 10
 
 
 def pivotArray(nums: list[int], pivot: int) -> list[int]:
-    indice_pivot = nums.index(10)
-    nb_inser = 0
-    nb_pivot = 0
-    i = 0
-    while i < len(nums):
-        if nums[i] == 0.5:
-            i += 1
+    inf = []
+    egal = []
+    sup = []
+    for i in nums:
+        if i < pivot:
+            inf.append(i)
+        elif i > pivot:
+            sup.append(i)
         else:
-            if i < indice_pivot:
-                if nums[i] > pivot:
-                    val = nums[i]
-                    nums[i] == 0.5
-                    nums.insert(indice_pivot+nb_inser+1, val)
-                    nb_inser += 1
-                elif nums[i] == pivot:
-                    val = nums[i]
-                    nums[i] == 0.5
-                    nums.insert(indice_pivot, val)
-                    nb_pivot += 1
-                else:
-                    i += 1
-            elif i > indice_pivot+nb_pivot:
-                if nums[i] < pivot:
-                    val = nums[i]
-                    nums[i] == 0.5
-                    nums.insert(indice_pivot, val)
-                elif nums[i] == pivot:
-                    val = nums[i]
-                    nums[i] == 0.5
-                    nums.insert(indice_pivot+1, val)
-                    nb_pivot += 1
-                else:
-                    i += 1
-            else:
-                i += 1
-            
-        print(nums)
+            egal.append(i)
     
-    return nums
+    
+    return inf + egal + sup
+            
+    
+    
+    
             
                 
 
