5925774b903315a28946977f6b6e614896948c21
Gauthier <gauthierbeaudoux@gmail.com>
2026-03-03 22:15:24 +0800

    Create 518_CoinChangeII.py

---
+++ 518_CoinChangeII.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+def change(amount: int, coins: list[int]) -> int:
+    
+    memo = defaultdict(int)
+    def dfs(i, total):
+        if total == 0:
+            return 1
+        elif total < 0:
+            return 0
+        
+        if (i, total) in memo:
+            return memo[(i, total)]
+        
+        
+        for j in range(i, len(coins)):
+            if total-coins[j] >= 0:
+                memo[(i, total)] += dfs(j, total-coins[j])
+            
+        return memo[(i, total)]
+
+    return dfs(0, amount)
+
+amount = 5
+coins = [1,2,5]
+print(change(amount, coins))
