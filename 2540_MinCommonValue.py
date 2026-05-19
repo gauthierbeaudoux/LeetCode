2714aaf0e46a883b199cb6708def4ef241723bcc
Gauthier <gauthierbeaudoux@gmail.com>
2026-05-19 20:33:26 +0800

    Update 2540_MinCommonValue.py

--- 2540_MinCommonValue.py
+++ 2540_MinCommonValue.py
@@ -1,11 +1,20 @@
-nums1 = [1,2,3]
-nums2 = [2,4]
+
 
 def getCommon(nums1: list[int], nums2: list[int]) -> int:
-    for i in nums1:
-        if i >= nums2[0] and i in nums2:
-            return i
+    i1 = 0
+    i2 = 0
+
+    while i1 < len(nums1) and i2 < len(nums2):
+        if nums1[i1] == nums2[i2]:
+            return nums1[i1]
+        elif nums1[i1] > nums2[i2]:
+            i2 += 1
+        else:
+            i1 += 1
+            
     return -1
 
-    
+
+nums1 = [1,2,3]
+nums2 = [2,4]
 print(getCommon(nums1, nums2))
