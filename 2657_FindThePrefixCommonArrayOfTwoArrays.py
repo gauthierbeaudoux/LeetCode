53a7555b4f6569b90b1629a597e5cd89f8420200
Gauthier <gauthierbeaudoux@gmail.com>
2026-05-21 00:13:29 +0800

    Update 2657_FindThePrefixCommonArrayOfTwoArrays.py

--- 2657_FindThePrefixCommonArrayOfTwoArrays.py
+++ 2657_FindThePrefixCommonArrayOfTwoArrays.py
@@ -1,17 +1,35 @@
-A = [1,3,2,4]
-B = [3,1,2,4]
+from collections import defaultdict
 
 def findThePrefixCommonArray(A: list[int], B: list[int]) -> list[int]:
     result = []
+    memo = defaultdict(int)
     
+    # for i in range(len(A)):
+    #     nbr = 0
+    #     for j in A[:i+1]:
+    #         if j in B[:i+1]:
+    #            nbr += 1
+    #     result.append(nbr)
+    
+    # return result 
+
+
+    curr = 0
     for i in range(len(A)):
-        nbr = 0
-        for j in A[:i+1]:
-            if j in B[:i+1]:
-               nbr += 1
-        result.append(nbr)
+        print(memo)
+        
+        curr += memo[A[i]]
+        memo[A[i]] += 1
+        
+        curr += memo[B[i]]
+        memo[B[i]] += 1
     
-    return result 
+        result.append(curr)
+        
+    return result
 
 
+
+A = [1,3,2,4]
+B = [3,1,2,4]
 print(findThePrefixCommonArray(A, B))
