d3be0ea9a5ce8e8fcb5a7432f73870b00a89e129
Gauthier <gauthierbeaudoux@gmail.com>
2026-07-27 21:05:26 +0800

    Update 1464_MaxProduct.py

--- 1464_MaxProduct.py
+++ 1464_MaxProduct.py
@@ -1,10 +1,18 @@
-nums = [3,4,5,2]
 
 def maxProduct(nums: list[int]) -> int:
-    a = max(nums)
-    nums.pop(nums.index(a))
-    b = max(nums)
-    return (a-1)*(b-1)
+    max1 = nums[0]
+    max2 = nums[1]
+    
+    if max1 < max2:
+        max1, max2 = max2, max1
+        
+    for i in range(2, len(nums)):
+        if nums[i] > max2:
+            max2 = nums[i]
+            if max1 < max2:
+                max1, max2 = max2, max1
+        
+    return (max1-1)*(max2-1)
 
-
+nums = [3,4,5,2]
 print(maxProduct(nums))
